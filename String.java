4b150d735851fcc30c5c7a1b716ada2968d8734c
Abu Naiim <75266387+abunaiim25@users.noreply.github.com>
2021-02-26 22:12:57 +0600

    Add files via upload

---
+++ String.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[] names = {"Abu","Naiim","Rayhan","Siddique"};
+
+        System.out.println(names.length);
+
+        for(int i=0; i<4;i++)
+        {
+            System.out.println(names[i]);
+        }
+
+
+    }
+}
